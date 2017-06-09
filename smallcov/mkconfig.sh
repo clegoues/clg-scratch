66c2847b9e5f8b729923091ac73d1b8e6c5936f9
Claire Le Goues <clegoues@cs.cmu.edu>
2017-06-09 12:44:57 -0400

    quieter compilation

--- smallcov/mkconfig.sh
+++ smallcov/mkconfig.sh
@@ -6,7 +6,7 @@ grep "pos-tests" configuration-default >> cov.config
 grep "neg-tests" configuration-default >> cov.config
 echo "--testscript ./test.sh" >> cov.config
 echo "--prefix preprocessed" >> cov.config
-echo "--compcmd ./compile.sh __EXE_NAME__" >> cov.config
+echo "--compcmd ./compile.sh __EXE_NAME__ 2>/dev/null >/dev/null" >> cov.config
 diffs=`cd diffs; find . -follow | grep "\-diff" | cut -c 3-`
 echo "--diffs $diffs" >> cov.config
 
